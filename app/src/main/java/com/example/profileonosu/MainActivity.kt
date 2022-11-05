d01efd4416f0f708abb9be4ff6164614add8411b
Alterys <krgtskrgts005@gmail.com>
2022-11-05 20:10:53 +0300

    fix ORIENTATION_LOCKED

--- app/src/main/java/com/example/profileonosu/MainActivity.kt
+++ app/src/main/java/com/example/profileonosu/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
         setContentView(R.layout.activity_main)
     }
 }
