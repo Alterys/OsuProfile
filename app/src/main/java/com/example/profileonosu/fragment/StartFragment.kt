d320ee3f28acf770c860373f1e93ffa2b399b617
Alterys <krgtskrgts005@gmail.com>
2022-11-14 22:56:02 +0300

    add internet connect

--- app/src/main/java/com/example/profileonosu/fragment/StartFragment.kt
+++ app/src/main/java/com/example/profileonosu/fragment/StartFragment.kt
@@ -1,5 +1,9 @@
 package com.example.profileonosu.fragment
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +30,11 @@ open class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentStartBinding.bind(view)
+        val internetConnect = checkForInternet()
+
         binding.search.setOnClickListener {
+            if (!internetConnect)
+                binding.error.text = ERROR_01
             val bundle = Bundle()
             val name = binding.textName.text
             bundle.putString(KEY_NICKNAME, name.toString())
@@ -34,6 +42,25 @@ open class StartFragment : Fragment() {
                 binding.error.text = ERROR_01
             else
                 findNavController().navigate(R.id.action_startFragment_to_endFragment, bundle)
+        }
+    }
+
+    private fun checkForInternet(): Boolean {
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
         }
     }
 }
